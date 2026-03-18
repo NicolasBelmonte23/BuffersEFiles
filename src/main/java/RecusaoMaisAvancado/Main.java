7dd076f14cda413ef8fd6559629dc67bcdaadb8d
Nicolas Belmonte <nico.belmonte2014@gmail.com>
2026-03-18 14:44:03 -0300

    testando o metodo criado palindromo

--- src/main/java/RecusaoMaisAvancado/Main.java
+++ src/main/java/RecusaoMaisAvancado/Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println(app.numeroPrimo(4));
         System.out.println(app.inverteString("nico"));
         System.out.println(app.palindromo("anas"));
+        System.out.println(app.palindromo("ana"));
     }
 }
