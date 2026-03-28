71b695d6b3685d89a76ee2a2b0c41d555ebe8711
Nicolas Belmonte <nico.belmonte2014@gmail.com>
2026-03-28 10:14:36 -0300

    desafio de busca sequencial da facul

--- src/main/java/pesquisaBinaria/Main.java
+++ src/main/java/pesquisaBinaria/Main.java
@@ -5,9 +5,11 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] ar = {1,7,3,4,5,6,2};
-        Arrays.sort(ar);//ordenando em ordem crescente
-        int va = 7;
-        System.out.println(binaria(ar,va));
+        int[] ar2 = {1,3,5,6,7};
+//        Arrays.sort(ar);//ordenando em ordem crescente
+//        int va = 7;
+//        System.out.println(binaria(ar,va));
+        System.out.println(sequencial(ar2,4));
     }
     public static int binaria(int[]arr,int valor) {
         int inferior = 0;
@@ -23,5 +25,15 @@ public class Main {
             }
         }
             return -1;
+    }
+    public static int sequencial(int[]x,int num){
+        for(int i = 0;(i < x.length)&&(x[i]<=num);i++){
+            if(num == x[i]){
+                System.out.printf("O numero %d foi encontrado.",num);
+                return num;
+            }
+        }
+        System.out.printf("O numero %d nao foi encontrado.",num);
+        return -1;
     }
 }
